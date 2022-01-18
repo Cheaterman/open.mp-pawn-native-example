ff17f2da8a77389db78b1427ca1cf76c502b6f94
The Cheaterman <the.cheaterman@gmail.com>
2022-01-18 06:58:31 +0100

    Add HelloWorld Pawn native

--- main.cpp
+++ main.cpp
@@ -1,10 +1,17 @@
 #include <sdk.hpp>
+#include <Server/Components/Pawn/pawn.hpp>
+#include <NativeFunc.hpp>
+#include <NativesMain.hpp>
 
-struct HelloComponent final : IComponent {
+struct HelloComponent;
+HelloComponent* helloComponent = nullptr;
+extern void *pAMXFunctions;
+
+struct HelloComponent final : IComponent, PawnEventHandler {
 	PROVIDE_UUID(0x1126fdb4780f11ec);
 
 	StringView componentName() const override {
-		return "Says hello";
+		return "HelloWorld";
 	}
 
 	SemanticVersion componentVersion() const override {
@@ -13,20 +20,39 @@ struct HelloComponent final : IComponent {
 
 	void onLoad(ICore* c) override {
 		core = c;
-		c->printLn("Hello, world!");
+		core->printLn("HelloWorld component loaded!");
 	}
 
-	void onInit(IComponentList* components) override {}
+	void onInit(IComponentList* components) override {
+		pawnComponent = components->queryComponent<IPawnComponent>();
+		pawnComponent->getEventDispatcher().addEventHandler(this);
+		pAMXFunctions = (void *)&pawnComponent->getAmxFunctions();
+	}
 
 	void onReady() override {}
 
+	void onAmxLoad(void* amx) override {
+		pawn_natives::AmxLoad((AMX *)amx);
+	}
+
+	void onAmxUnload(void* amx) override {}
+
 	void free() override {
+		pawnComponent->getEventDispatcher().removeEventHandler(this);
 		delete this;
 	}
 
 	ICore* core = nullptr;
+	IPawnComponent* pawnComponent;
 };
 
 COMPONENT_ENTRY_POINT() {
-	return new HelloComponent();
+	helloComponent = new HelloComponent();
+	return helloComponent;
+}
+
+PAWN_NATIVE(Hello, HelloWorld, bool())
+{
+	helloComponent->core->printLn("Hello, world!");
+	return true;
 }
